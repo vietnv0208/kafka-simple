a6aea573af34222fe28c319212810d40d64aa88f
VietNV <vietnv@twendeesoft.com>
2022-12-13 14:39:49 +0700

    move

---
+++ src/test/java/com/example/kafkasimple/KafkaSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkasimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
